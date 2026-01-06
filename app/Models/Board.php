9604c8b8930f9ec4450799ab2686756f76fd453e
antaress <nanatchendopjoelivan@gmail.com>
2026-01-06 04:29:07 +0100

    Add models, migrations, and factories for boards, tasks, and invitations

---
+++ app/Models/Board.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+
+class Board extends Model
+{
+    /** @use HasFactory<\Database\Factories\BoardFactory> */
+    use HasFactory;
+
+    public $fillable = ['title', 'color', 'user_id'];
+
+    public function user(): BelongsTo {
+        return $this->belongsTo(User::class);
+    }
+
+    public function tasks(): HasMany {
+        return $this->hasMany(Task::class);
+    }
+}
