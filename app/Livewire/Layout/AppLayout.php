6a1e3d1305226742453f12390afbdd1d58ac494d
antaress <nanatchendopjoelivan@gmail.com>
2026-01-07 09:09:40 +0100

    feat: enhance app layout with responsive navigation and user components; add board routing and index page

---
+++ app/Livewire/Layout/AppLayout.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Livewire\Layout;
+
+use Livewire\Attributes\Layout;
+use Livewire\Component;
+
+#[Layout('components.layouts.app')]
+class AppLayout extends Component
+{
+
+}
--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,7 @@ use App\Livewire\Pages\Auth\Login;
 use App\Livewire\Pages\Auth\Register;
 use Illuminate\Support\Facades\Route;
 use Symfony\Component\Routing\Router;
+use App\Livewire\Pages\Board\IndexPage;
 
 Route::get('/', function () {
     return view('welcome');
@@ -12,4 +13,8 @@ Route::get('/', function () {
 Route::prefix('')->middleware(['guest'])->group(function () {
     Route::get('register', Register::class)->name('register');
     Route::get('login', Login::class)->name('login');
+});
+
+Route::prefix('board')->middleware(['auth'])->group(function () {
+    Route::get('/', IndexPage::class)->name('board.index');
 });
